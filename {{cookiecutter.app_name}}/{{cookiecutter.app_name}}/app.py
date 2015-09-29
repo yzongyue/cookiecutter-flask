ac39ce22dd1034e3b3e0a6b9d589af2f9521dc86
Will Mcginnis <wdm0006@auburn.edu>
2015-09-28 20:59:45 -0400

    added live example and fixed some javascript loading

--- {{cookiecutter.app_name}}/{{cookiecutter.app_name}}/app.py
+++ {{cookiecutter.app_name}}/{{cookiecutter.app_name}}/app.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """The app module, containing the app factory function."""
 from flask import Flask, render_template
-from flask.ext.heroku import Heroku
+import os
 
 from {{cookiecutter.app_name}}.settings import ProdConfig
 from {{cookiecutter.app_name}}.assets import assets
@@ -27,7 +27,7 @@ def create_app(config_object=ProdConfig):
     app.config.from_object(config_object)
 
     if config_object == ProdConfig:
-        heroku = Heroku(app)
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 
     register_extensions(app)
     register_blueprints(app)
