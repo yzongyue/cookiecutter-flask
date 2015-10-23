4ef4c09d6d75cbbf2cacd73ec20dbc47a2c77ff5
Will Mcginnis <will@predikto.com>
2015-10-22 20:05:54 -0400

    blog editing admin page added

--- {{cookiecutter.app_name}}/{{cookiecutter.app_name}}/views/admin.py
+++ {{cookiecutter.app_name}}/{{cookiecutter.app_name}}/views/admin.py
@@ -38,3 +38,40 @@ def new_blog():
         current_user.save()
 
     return render_extensions('admin/new_blog.html')
+
+@blueprint.route("/edit_blog/<blog_id>/", methods=["GET", "POST"])
+@login_required
+def edit_blog(blog_id):
+    if not current_user.is_admin:
+        return render_extensions('401.html')
+
+    if request.method == 'POST':
+        try:
+            content = str(request.form['content'])
+        except Exception:
+            content = ''
+
+        try:
+            slug = str(request.form['slug'])
+        except Exception:
+            slug = ''
+
+        try:
+            title = str(request.form['title'])
+        except Exception:
+            title = ''
+
+        post = Post(title=title, body=content, slug=slug)
+        post.save()
+
+        current_user.posts.append(post)
+        current_user.save()
+
+    post_obj = Post.query.filter_by(id=int(blog_id)).first()
+    post_content = {
+        'title': str(post_obj.title),
+        'slug': str(post_obj.slug),
+        'body': str(post_obj.body),
+    }
+
+    return render_extensions('admin/edit_blog.html', post=post_content)
